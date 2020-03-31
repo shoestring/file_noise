1ee3634f141e73c7516eecc3aae00d245cfdf47c
Mike Cammilleri <mjcammilleri@wisc.edu>
2020-03-31 17:08:29 -0500

    updates for today

--- file_noise.py
+++ file_noise.py
@@ -1,30 +1,38 @@
+#!/usr/bin/python3
+#
+# Generate random files in a temp directory of a certain size.
+# Repeat for specified time.
+#
+# Mike Cammilleri  Tue Mar 31 13:25:38 CDT 202
+#
 import subprocess
-from os import path
+import os
 import argparse
 import tempfile
 
-# TO DO Create argparse options with defaults for path name, file size, and time to run.
+# Create random string for directory and cd to it.
+# Create argparse options with defaults for path name, file size, and time to run.
+td = tempfile.TemporaryDirectory()
+randomdir = td.name
+os.chdir(randomdir)
 
-filename = tempfile.NamedTemporaryFile()
-parser = argparse.ArgumentParser(description='Process some integers')
-parser.add_argument('-p', '--path', required=False, default='/tmp/randomfiles', help='Set the path where the random files should exist.')
-parser.add_argument('-s', '--file', required=False, default=1024, help='Specify size of each file.')
-parser.add_argumet('-t', '--time', required=False, default=10, help='Specify length of time to run.')
+parser = argparse.ArgumentParser(description='Create random files of size repeatedly until done.')
+parser.add_argument('-p', '--path', required=False, default=randomdir, help='Set the path where the random\
+ files should exist.')
+parser.add_argument('-s', '--filesize', required=False, default=1024, help='Specify size of each file.')
+parser.add_argument('-t', '--time', required=False, default=10, help='Specify length of time to run.')
 args = parser.parse_args()
 
+path = args.path
+filesize = args.file
+time = args.time
 
-# Create temporary directory with random name
-
-with tempfile.TemporaryDirectory() as td:
-    os.mkdir(td)
-
-
-
-
-# TO DO Create files of certain size (big-ish) perhaps by streaming data into them?
-# Can use subprocess 'dd' to create empty files of 'size.' Randomize file names using
-# random().
-bigfiles = subprocess.run(['/bin/dd', 'if=/dev/zero', 'of=./data.bin', 'count=400', 'bs=1024'])
+# TO DO Create files of certain size (big-ish)
+# TO DO filename needs to be a string or something that subprocess.run can read in. check conversions
+for i in range(5):
+    tfn = tempfile.NamedTemporaryFile(dir='.')
+    filename = str(tfn.name)
+    bigfiles = subprocess.run(['/bin/dd', 'if=/dev/zero', 'of=', filename, 'count=400', 'bs=', str(filesize)])
 
 # TO DO Then remove files of certain age, or perhaps after they grow to X size. This
 # loop should not surpass a certain max_size.
