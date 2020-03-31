8d43263241690cd04d77f200b61760ed3beee062
Mike Cammilleri <mjcammilleri@wisc.edu>
2020-03-30 21:06:46 -0500

    adding first file.

---
+++ file_noise.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+# TO DO Check if a directory exists, if not create it. If it does exist, handle it.
+
+# TO DO Create files of certain size (big-ish) perhaps by streaming data into them?
+
+# TO DO Then remove files of certain age, or perhaps after they grow to X size. This
+# loop should not surpass a certain max_size.
+
